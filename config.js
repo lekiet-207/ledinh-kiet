307ee9a729c532aab136c34486558097e674580b
ledinh-kiet <kietle161207@gmail.com>
2024-10-20 00:34:21 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+const nameGirl = 'By kiệt';
+const giftUrl = 'https://zalo.me/0559109175';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng các bn';
+const contentCard = 'Các bạn gái lớp mình, những bông hoa tươi tắn của lớp mình! Khoảng thời gian cấp ba thật đáng nhớ với biết bao kỷ niệm vui buồn. Giờ đây, cánh cửa đại học đang rộng mở chào đón các bạn. Hãy luôn nhớ rằng, dù có khó khăn gì đi nữa, các bạn vẫn luôn có thầy/cô và bạn bè bên cạnh. Chúc các em thi tốt và đạt được những ước mơ của mình! văn này nó hơi sến thông cảm k biết viết nên nhờ AI ❤️'
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'kietn.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
