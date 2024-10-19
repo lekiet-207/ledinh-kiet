811290334f4e90f7eb553359b05aa19a879b2f56
lekiet-207 <121106171+lekiet-207@users.noreply.github.com>
2024-10-20 01:16:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'By kiệt';
 const giftUrl = 'https://zalo.me/0559109175';
 const eventName = 'Chúc Mừng 20-10';
 const titleCard = 'Tặng các bn';
-const contentCard = 'Các bạn gái lớp mình, những bông hoa tươi tắn của lớp mình! Khoảng thời gian cấp ba thật đáng nhớ với biết bao kỷ niệm vui buồn. Giờ đây, cánh cửa đại học đang rộng mở chào đón các bạn. Hãy luôn nhớ rằng, dù có khó khăn gì đi nữa, các bạn vẫn luôn có thầy/cô và bạn bè bên cạnh. Chúc các bn thi tốt và đạt được những ước mơ của mình! ❤️'
+const contentCard = 'Các bạn gái lớp mình, những bông hoa tươi tắn của lớp mình! Khoảng thời gian cấp ba thật đáng nhớ với biết bao kỷ niệm vui buồn. Giờ đây, cánh cửa đại học đang rộng mở chào đón các bạn. Hãy luôn nhớ rằng, dù có khó khăn gì đi nữa, các bạn vẫn luôn có thầy/cô và bạn bè bên cạnh. Chúc các bn thi tốt và đạt được những ước mơ của mình!  ❤️'
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
